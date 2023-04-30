4eeac0a3be40358ffefa8ab39d08501606c378fb
DENSLIN <denslinnunes2@gmail.com>
2023-04-30 12:32:46 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Explore from "./components/Explore";
+import Login from "./components/Login";
+import Register from "./components/Register"
+import Profile from "./components/Profile";
+import CreateEvent from "./components/CreateEvent";
+import {useState} from "react";
+import AddEvent from "./components/AddEvents";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const[User,setUser]=useState();
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Navbar user={User}/>}>
+                        <Route index element={<Home/>}/>
+                        <Route path="Explore" element={<Explore/>}/>
+                        {User && <Route path="Profile" element={<Profile/>} />}
+                        {User && <Route path="myevents" element={<CreateEvent user = {User}/>} />}
+                    </Route>
+                    <Route path="Login" element={<Login setUser={setUser} />}/>
+                    <Route path="Register" element={<Register setUser={setUser} />}/>
+                    <Route path="AddEvents" element={<AddEvent user = {User}/>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
+
 
 export default App;
